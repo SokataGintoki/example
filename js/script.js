5d82eb96b8585cdca352a22112015c74ef7a03a4
Benizakura <example@gmail.com>
2022-12-19 18:31:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18 лет?', '18');
+// console.log(answer + 14);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// alert ('BOOM');
