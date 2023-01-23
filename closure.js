b925ed5234f9b5f0f2fbb19e7007d0037fee8938
Benizakura <example@gmail.com>
2023-01-23 21:33:32 +0300

    Получение элементов со страницы

--- closure.js
+++ closure.js
@@ -1,12 +1,30 @@
 'use strict';
 
-let number = 5; debugger
+// let number = 5; debugger
 
-function logNumber() {
-    let number = 4; debugger
-    console.log(number);
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function(){
+        counter = counter + 1;
+        return counter;
+    }
+
+    return myFunction;
 }
 
-number = 6;
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
 
-logNumber(); debugger
+console.log(c1, c2, c3);
---
+++ example_interview.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let x = 5; alert(x++);
+// [ ] +false - null + true;
+console.log(typeof([] + false)); // "false" // NaN
+
+// let y = 1; 
+//     x = y = 2;
+// alert(x);
+
+console.log([] + 1 + 2);  // '12'
+
+alert('1' [0] ); // '1'
+
+console.log(2 && 1 &&  0 && undefined && null ); // "и" запинается на "лжи"
+
+console.log( !!( a && b) === (a && b));
+
+console.log( null || 2 && 3 || 4); // "или" запинается на правде
+
+// const a = [1, 2, 3];
+//       b = [1, 2, 3];
+// console.log(a == b);
+
+// alert(+ "Infinity");
+
+// console.log('Ёжик'>'Яблоко');
+
+console.log(0 || '' || 2 || undefined || true || false);
