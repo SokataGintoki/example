2ddd078038d41f33fb0a0a0f93c8bf4999c077fc
Benizakura <example@gmail.com>
2023-01-30 18:44:26 +0300

    Событие на мобильных телефонах

---
+++ Azbuka/eventonphone.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// touchstart - событие срабатывает при возникновении касания к этому элементу
+// touchmove - при касании к элементу начинает двигаться палец, срабатывает эта событие
+// touchend - как только наш палец оторвался от элемента, срабатывает это событие
+// touchenter - оно будет срабатывать, когда мы ведем пальцем по экрану и наскакиванем на предел этого элемента
+// touchleave - когда наш палец, не оторвался от экрана, а продолжил сколзить и ушел за пределы элемента
+// touchcancel - возникает тогда, когде точка соприкосновения, больше не регистрируется на поверхности
+
+window.addEventListener('DOMContentLoaded', () => {
+   const box = document.querySelector('.box');
+
+   box.addEventListener('touchstart', (event) =>{
+       event.preventDefault();
+       console.log('Start');
+       console.log(event.touches);
+   });
+   box.addEventListener('touchmove', (event) =>{
+    event.preventDefault();
+    console.log('Move');
+});
+   box.addEventListener('touchend', (event) =>{
+    event.preventDefault();
+    console.log('End');
+});
+});
+
+// touches - выдает список всех пальцев, которые сейчас взаимодействуют с экраном
